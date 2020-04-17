21577494bf37edb8f8d52b0ed49d955aefc484b2
unknown <ashaban7642@gmail.com>
2020-04-17 13:14:35 +0200

    add new features to the forcast

--- src/utilis/forcast.js
+++ src/utilis/forcast.js
@@ -8,7 +8,7 @@ const forcast = (longitude , latitude , callback)=>{
         }else if(body.error){
             callback('unable to find the location!')
         }else{
-            callback(undefined , body.daily.data[0].summary + ' its currently ' + body.currently.temperature + ' degrees out. there is ' + body.currently.precipProbability + ' chance of rain')
+            callback(undefined , body.daily.data[0].summary + ' its currently ' + body.currently.temperature + ' degrees out. the high is ' + body.daily.data[0].temperatureHigh + ' and the law is '+ body.daily.data[0].temperatureLow + '. there is ' + body.currently.precipProbability + ' chance of rain')
         }
     })
 }
